3f5f4c785a91483203c7763e522c5375ee74ca0b
Bastian Burger <b.burger@gmx.eu>
2017-01-03 11:52:09 +0100

    Database v1 introduced

--- app/models.py
+++ app/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Device(models.Model):
+        device = models.IntegerField()
+        time_stamp = models.IntegerField()
+        temp = models.DecimalField(max_digits=4, decimal_places=1)
+        hum = models.DecimalField(max_digits=4, decimal_places=1)
+        bar = models.DecimalField(max_digits=5, decimal_places=1)
+
+        def __str__(self):
+            return self.device
--- app/views.py
+++ app/views.py
@@ -1,32 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Device
 
 # Create your views here.
 def index(request):
-    device = "Sensor 1"
-    temp = 20.0
-    hum = 45.5
-    bar = 989.7
-
-    context = {"device": device,
-    "temperature": temp,
-    "humidity": hum,
-    "pressure": bar}
-
+    devices = Device.objects.all()
     return render(request, "index.html", {"devices":devices})
 
-
-class Device:
-    def __init__(self, id, time_stamp, temp, hum, bar):
-        self.id = id
-        self.time_stamp = time_stamp
-        self.temp = temp
-        self.hum = hum
-        self.bar = bar
-
-devices = [
-    Device(1, 12, 20.2, 45.5, 989.7),
-    Device(3, 12, 20.0, 55.2, 923.9),
-    Device(7, 15, 22.6, 43.6, 1011.1),
-    Device(1, 16, 20.3, 47.3, 998.4)
-]
+#devices = [
+#    Device(device = 1, time_stamp = 12, temp = 20.2, hum = 45.5, bar = 989.7),
+#    Device(device = 3, time_stamp = 12, temp = 20.0, hum = 55.2, bar = 923.9),
+#    Device(device = 7, time_stamp = 15, temp = 22.6, hum = 43.6, bar = 1011.1),
+#    Device(device = 1, time_stamp = 16, temp = 20.3, hum = 47.3, bar = 998.4)
+#]
